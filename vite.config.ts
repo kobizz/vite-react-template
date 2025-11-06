7527798aca33f6ce5f3f5ce75337797520438c21
Kobi Zaltzberg <jzaltzberg@gmail.com>
2025-11-06 11:52:21 +0200

    change

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   server: {
     port: 8000,
     strictPort: true,
-    allowedHosts: 'all',
+    allowedHosts: true,
     // proxy: {
     //   '/api': {
     //     target: 'http://127.0.0.1:3080',
